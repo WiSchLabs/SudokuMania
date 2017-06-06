1e755dc8bb4ab746bef602bf5ce3bbc6be02c5bc
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-06-06 20:11:42 +0200

    fix project structure

---
+++ src/test/kotlin/sudoklin/HelloTest.kt
@@ -0,0 +1,10 @@
+package sudoklin
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class HelloTest {
+    @Test fun testAssert() : Unit {
+        assertEquals("Hello, world!", getHelloString())
+    }
+}
