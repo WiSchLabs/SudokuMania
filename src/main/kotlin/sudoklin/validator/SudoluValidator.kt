5981e51c720e1699a94fed065cd9e6fa60954060
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-06-26 18:52:58 +0200

    first test for validator - test valid row

---
+++ src/main/kotlin/sudoklin/validator/SudoluValidator.kt
@@ -0,0 +1,13 @@
+package sudoklin.validator
+
+import sudoklin.data.Sudoku
+
+/**
+ * Created by sebastian on 26.06.17.
+ */
+class SudokuValidator {
+    fun validateRow(sudoku: Sudoku, rowNumber: Int): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ src/test/kotlin/sudoklin/validator/SudokuValidatorTest.kt
@@ -0,0 +1,23 @@
+package sudoklin.validator
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import sudoklin.data.Sudoku
+import sudoklin.impex.SudokuFileImporter
+import kotlin.test.assertTrue
+
+/**
+ * Created by sebastian on 26.06.17.
+ */
+class SudokuFileImporterTest : Spek({
+    describe("Given a correctly filled row") {
+        val file_importer = SudokuFileImporter()
+        val sudoku = file_importer.import("src/test/resources/filled_valid.sdk")
+
+        it("the validator should return that it is valid") {
+            val validator = SudokuValidator()
+            assertTrue(validator.validateRow(sudoku, 1))
+        }
+    }
+})
