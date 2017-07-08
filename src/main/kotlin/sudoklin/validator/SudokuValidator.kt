d4129f2edff0cdcf15a2a386d900f98d08a01071
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-07-08 15:36:24 +0200

    refactorio

--- src/main/kotlin/sudoklin/validator/SudokuValidator.kt
+++ src/main/kotlin/sudoklin/validator/SudokuValidator.kt
@@ -3,32 +3,26 @@ package sudoklin.validator
 import sudoklin.data.Sudoku
 
 class SudokuValidator {
-    fun validateRow(sudoku: Sudoku, rowNumber: Int): Boolean {
-        val row = sudoku.puzzle.getRow(rowNumber)
-        var valid = true
-        for (i in 1..9) {
-            val filteredRow = row.filter { it == i.toString() }
-            valid = valid && (filteredRow.size <= 1)
-        }
-        return valid
+    fun validateRow(sudoku: Sudoku, rowIndex: Int): Boolean {
+        val row = sudoku.puzzle.getRow(rowIndex)
+        return validateList(row)
     }
 
-    fun validateColumn(sudoku: Sudoku, columnNumber: Int): Boolean {
-        val column = sudoku.puzzle.getColumn(columnNumber)
-        var valid = true
-        for (i in 1..9) {
-            val filteredColumn = column.filter { it == i.toString() }
-            valid = valid && (filteredColumn.size <= 1)
-        }
-        return valid
+    fun validateColumn(sudoku: Sudoku, columnIndex: Int): Boolean {
+        val column = sudoku.puzzle.getColumn(columnIndex)
+        return validateList(column)
     }
 
     fun validateGroup(sudoku: Sudoku, groupIndex: Int): Boolean {
-        val column = sudoku.puzzle.getGroup(groupIndex)
+        val group = sudoku.puzzle.getGroup(groupIndex)
+        return validateList(group)
+    }
+
+    private fun validateList(list: List<String>): Boolean {
         var valid = true
         for (i in 1..9) {
-            val filteredColumn = column.filter { it == i.toString() }
-            valid = valid && (filteredColumn.size <= 1)
+            val filteredList = list.filter { it == i.toString() }
+            valid = valid && (filteredList.size <= 1)
         }
         return valid
     }
