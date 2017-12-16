eabb06350c1d737bf035ecfc0cae89c64eb6b986
Michael Wittig <witmic1@gmail.com>
2017-12-16 19:41:45 +0100

    Fix premature optimization by kotlin

--- src/test/kotlin/sudoklin/solver/SudokuSolverCombinationsFindingTest.kt
+++ src/test/kotlin/sudoklin/solver/SudokuSolverCombinationsFindingTest.kt
@@ -12,17 +12,23 @@ class SudokuSolverCombinationsFindingTest : Spek({
     describe("A sudoku") {
         val fileImporter = SudokuFileImporter()
 
-        it("should eliminate all occurrences of found pairs") {
-            it("in row") {
+        describe("should eliminate all occurrences of found ") {
+            it("pairs in row") {
                 val sudoku = fileImporter.import("src/test/resources/tuple_candidate_elimination_for_row.sdk")
+                val solver = SudokuSolver(sudoku)
+                solver.findPairsOfCandidatesAndEliminateOthers()
+
                 assertTrue((2..8).all { columnIndex ->
                     !sudoku.getCell(0, columnIndex).candidates.contains(1) &&
                     !sudoku.getCell(0, columnIndex).candidates.contains(2)
                 })
             }
 
-            it("in column") {
+            it("pairs in column") {
                 val sudoku = fileImporter.import("src/test/resources/tuple_candidate_elimination_for_column.sdk")
+                val solver = SudokuSolver(sudoku)
+                solver.findPairsOfCandidatesAndEliminateOthers()
+
                 assertTrue((2..8).all { rowIndex ->
                     !sudoku.getCell(rowIndex, 0).candidates.contains(1) &&
                     !sudoku.getCell(rowIndex, 0).candidates.contains(2)
@@ -31,6 +37,9 @@ class SudokuSolverCombinationsFindingTest : Spek({
 
             it("in group") {
                 val sudoku = fileImporter.import("src/test/resources/tuple_candidate_elimination_for_row.sdk")
+                val solver = SudokuSolver(sudoku)
+                solver.findPairsOfCandidatesAndEliminateOthers()
+
                 assertFalse(sudoku.getCell(0, 2).candidates.contains(1))
                 assertFalse(sudoku.getCell(0, 2).candidates.contains(2))
 
@@ -39,6 +48,8 @@ class SudokuSolverCombinationsFindingTest : Spek({
 
                 assertFalse(sudoku.getCell(2, 2).candidates.contains(1))
                 assertFalse(sudoku.getCell(2, 2).candidates.contains(2))
+
+
             }
 
         }
