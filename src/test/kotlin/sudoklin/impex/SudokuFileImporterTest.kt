ffcf9915a361d6a9162e1e676d985aed071bace5
Michael Wittig <witmic1@gmail.com>
2017-12-16 17:40:16 +0100

    re-implemented fileImporterTest

--- src/test/kotlin/sudoklin/impex/SudokuFileImporterTest.kt
+++ src/test/kotlin/sudoklin/impex/SudokuFileImporterTest.kt
@@ -2,9 +2,29 @@ package sudoklin.impex
 
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import sudoklin.data.Sudoku
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class SudokuFileImporterTest : Spek({
     describe("An importer") {
-        
+        val fileImporter = SudokuFileImporter()
+
+        it("should return a Sudoku object") {
+            val sudoku = fileImporter.import("src/test/resources/example.sdk")
+            assertTrue(sudoku is Sudoku)
+        }
+
+        it("should return a Sudoku object with first cell filled correctly") {
+            val sudoku = fileImporter.import("src/test/resources/example.sdk")
+            assertTrue(sudoku.getCell(0, 0).isSolvedWithNumber(2))
+        }
+
+        it("should return a Sudoku object with correct candidates for an unsolved cell") {
+            val sudoku = fileImporter.import("src/test/resources/example.sdk")
+            assertTrue(sudoku.getCell(0, 1).candidates.containsAll(listOf(6, 7, 9)))
+            assertEquals(3, sudoku.getCell(0, 1).candidates.size)
+        }
     }
 })
