a8c5c2c21110fc32e12be544f9965dfbc3eafb9d
Michael Wittig <witmic1@gmail.com>
2017-12-16 17:30:55 +0100

    Remove old data structures

--- src/main/kotlin/sudoklin/data/Sudoku.kt
+++ src/main/kotlin/sudoklin/data/Sudoku.kt
@@ -1,110 +1,11 @@
 package sudoklin.data
 
-class Sudoku (val puzzle: SudokuPuzzle){
+class Sudoku(val rows: Array<SudokuRow?> = Array(9, { _ -> null}),
+             val columns: Array<SudokuColumn?> = Array(9, {_ -> null}),
+             val groups: Array<SudokuGroup?> = Array(9, {_ -> null})) {
+
     fun clone(): Sudoku {
-        return Sudoku(SudokuPuzzle(puzzle.matrix))
-    }
-}
-
-class SudokuPuzzle (val matrix: Array<Array<IntArray>>) {
-    fun getCell(rowIndex: Int, columnIndex: Int): IntArray {
-        return matrix[rowIndex][columnIndex]
-    }
-
-    fun fixedNumberIsInCell(rowIndex: Int, columnIndex: Int, number: Int): Boolean {
-        val cell = getCell(rowIndex, columnIndex)
-        return cellIsFixed(rowIndex, columnIndex) && cell[0] == number
-    }
-
-
-    fun cellIsFixed(rowIndex: Int, columnIndex: Int): Boolean {
-        val cell = getCell(rowIndex, columnIndex)
-        return cell.size == 1
-    }
-
-    fun fixedNumberIsInColumn(columnIndex: Int, number: Int): Boolean {
-        return (0..8).any { fixedNumberIsInCell(it, columnIndex, number) }
-    }
-
-    fun fixedNumberIsInRow(rowIndex: Int, number: Int): Boolean {
-        return (0..8).any { fixedNumberIsInCell(rowIndex, it, number) }
-    }
-
-    fun fixedNumberIsInGroup(groupIndex: Int, number: Int): Boolean {
-        val group = getGroup(groupIndex)
-        return group.any { it.size == 1 && it[0] == number }
-    }
-
-
-    fun getRow(rowIndex: Int): Array<IntArray> {
-        return matrix[rowIndex]
-    }
-
-    fun getColumn(columnIndex: Int): Array<IntArray> {
-        val column: Array<IntArray> = Array<IntArray>(9, { IntArray(9) })
-        for (rowIndex in 0..8) {
-            column[rowIndex] = matrix[rowIndex][columnIndex]
-        }
-        return column
-    }
-
-    fun getGroup(groupIndex: Int): Array<IntArray> {
-        val group: MutableList<IntArray> = mutableListOf<IntArray>()
-        for (i in 0..2) {
-            val rowIndex = i + ((groupIndex / 3) * 3)
-            for (j in 0..2) {
-                val columnIndex = j + ((groupIndex % 3) * 3)
-                group.add(matrix[rowIndex][columnIndex])
-            }
-        }
-        return group.toTypedArray()
-    }
-
-    fun getGroupIndexForCell(rowIndex: Int, columnIndex: Int): Int {
-        var groupIndex = columnIndex / 3
-        groupIndex += (rowIndex / 3) * 3
-        return groupIndex
-    }
-
-    fun isSolved(): Boolean {
-        for (rowIndex in 0..8) {
-            for (columnIndex in 0..8) {
-                if (matrix[rowIndex][columnIndex].size > 1) {
-                    return false
-                }
-            }
-        }
-        return true
-    }
-
-    override fun toString(): String {
-        var s = "\r\n# # # # # # # # # # # # #\r\n"
-        for (rowIndex in 0..8) {
-            s += "#"
-            for (columnIndex in 0..8) {
-                s += " "
-                s += getCell(rowIndex, columnIndex).toString()
-                if (columnIndex == 2 || columnIndex == 5) {
-                    s += " |"
-                }
-            }
-            s += " #\r\n"
-
-            if (rowIndex == 2 || rowIndex == 5) {
-                s += "# - - - + - - - + - - - #\r\n"
-            }
-        }
-        s += "# # # # # # # # # # # # #\r\n"
-        return s
-    }
-}
-
-class NewShinySudoku(val rows: Array<SudokuRow?> = Array(9, {_ -> null}),
-                     val columns: Array<SudokuColumn?> = Array(9, {_ -> null}),
-                     val groups: Array<SudokuGroup?> = Array(9, {_ -> null})) {
-
-    fun clone(): NewShinySudoku {
-        return NewShinySudoku(rows, columns, groups)
+        return Sudoku(rows, columns, groups)
     }
 
     init {
--- src/test/kotlin/sudoklin/impex/SudokuFileImporterTest.kt
+++ src/test/kotlin/sudoklin/impex/SudokuFileImporterTest.kt
@@ -2,30 +2,9 @@ package sudoklin.impex
 
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import sudoklin.data.Sudoku
-import sudoklin.data.SudokuPuzzle
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class SudokuFileImporterTest : Spek({
     describe("An importer") {
-        val fileImporter = SudokuFileImporter()
-
-        it("should return a Sudoku object") {
-            val sudoku = fileImporter.import("src/test/resources/example.sdk")
-            assertTrue(sudoku is Sudoku)
-        }
-
-        it("should return a Sudoku object with a puzzle") {
-            val sudoku = fileImporter.import("src/test/resources/example.sdk")
-            assertTrue(sudoku.puzzle is SudokuPuzzle)
-        }
-
-        it("should return a Sudoku object with a puzzle with first cell filled correctly") {
-            val sudoku = fileImporter.import("src/test/resources/example.sdk")
-            assertEquals(2, sudoku.puzzle.getCell(0, 0)[0])
-            assertEquals(1, sudoku.puzzle.getCell(0, 0).size)
-        }
+        
     }
 })
