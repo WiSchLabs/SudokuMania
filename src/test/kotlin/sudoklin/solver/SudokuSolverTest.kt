ff0dfe871dd8968669b487c7bd707d09b9877215
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-12-17 15:14:04 +0100

    Replace ambiguous medium puzzle with solvable one

--- src/test/kotlin/sudoklin/solver/SudokuSolverTest.kt
+++ src/test/kotlin/sudoklin/solver/SudokuSolverTest.kt
@@ -115,15 +115,15 @@ class SudokuSolverTest : Spek({
                     assertTrue(solvedSudoku.isValid())
                 }
 
-//                it("should be able to solve an medium puzzle") {
-//                    val sudoku = fileImporter.import("src/test/resources/medium_sudoku.sdk")
-//
-//                    val solver = SudokuSolver(sudoku)
-//                    val solvedSudoku = solver.solve()
-//
-//                    assertTrue(solvedSudoku.isSolved())
-//                    assertTrue(solvedSudoku.isValid())
-//                }
+                it("should be able to solve an medium puzzle") {
+                    val sudoku = fileImporter.import("src/test/resources/medium_sudoku.sdk")
+
+                    val solver = SudokuSolver(sudoku)
+                    val solvedSudoku = solver.solve()
+
+                    assertTrue(solvedSudoku.isSolved())
+                    assertTrue(solvedSudoku.isValid())
+                }
 
                 it("should be able to solve an hard puzzle") {
                     val sudoku = fileImporter.import("src/test/resources/hard_sudoku.sdk")
